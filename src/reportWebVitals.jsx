f6de31c1292ccf6e3866f9e8184ed23afb87ec61
Sp-Rajjputt <sp.rajjputt@gmail.com>
2025-04-17 23:26:45 +0530

    first commit

--- src/reportWebVitals.jsx
+++ src/reportWebVitals.jsx
@@ -1,14 +1,14 @@
-const reportWebVitals = onPerfEntry => {
-    if (onPerfEntry && onPerfEntry instanceof Function) {
-      import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-        getCLS(onPerfEntry);
-        getFID(onPerfEntry);
-        getFCP(onPerfEntry);
-        getLCP(onPerfEntry);
-        getTTFB(onPerfEntry);
-      });
-    }
-  };
-  
-  export default reportWebVitals;
-  
+// reportWebVitals.jsx
+const reportWebVitals = async (onPerfEntry) => {
+  if (onPerfEntry && typeof onPerfEntry === 'function') {
+    const webVitals = await import('web-vitals');
+
+    webVitals.onCLS(onPerfEntry);
+    webVitals.onFID(onPerfEntry);
+    webVitals.onLCP(onPerfEntry);
+    webVitals.onINP(onPerfEntry);
+    webVitals.onTTFB(onPerfEntry);
+  }
+};
+
+export default reportWebVitals;
